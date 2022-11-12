aa39429ec3f0c50194e0b928dc55f035388785aa
lovermarmaris <lovermarmaris@yahoo.com>
2022-11-12 23:08:43 +0300

    5. Создать классы Собака и Кот с наследованием от класса Животное; 6. Животные могут выполнять действия: бежать, плыть, перепрыгивать препятствие. В качестве параметра каждому методу передается величина, означающая или длину препятствия (для бега и плавания), или высоту (для прыжков); 7. У каждого животного есть ограничения на действия (бег: кот – 200 м., собака – 500 м.; прыжок: кот – 2 м., собака – 0.5 м.; плавание: кот не умеет плавать, собака – 10 м.); 8. При попытке животного выполнить одно из этих действий, оно должно сообщить результат в консоль. (Например, dog1.run(150); -> результат: run: true);

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal dog1 = new Dog("Sharik");
+        dog1.animalInfo();
+        System.out.print(" is runnig " + dog1.run(222));
+        System.out.print(" is jumping " + dog1.jump(10));
+        System.out.print(" is swimming " + dog1.swim(10) + "\n");
+        Animal cat1 = new Cat("Barsik");
+        cat1.animalInfo();
+        System.out.print(" is runnig " + cat1.run(130));
+        System.out.print(" is jumping " + cat1.jump(2));
     }
 }
